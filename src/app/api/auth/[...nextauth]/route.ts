b0789d72b65812caed4c75951204da9408ea8da0
shantan <xojaniijan1988@gmail.com>
2023-12-30 20:44:13 +0900

    Initial commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,22 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  providers: [1
+  adapter: PrismaAdapter(prisma),
+  providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId: process.env.Google_CLIENT_ID ?? "",
+      clientSecret: process.env.Google_CLIENT_SECRET ?? "",
     }),
   ],
 };
